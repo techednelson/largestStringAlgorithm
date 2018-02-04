71879b0be703d40a28789e2cc53843b392796db0
Nelson Sandoval <techednelson@gmail.com>
2018-02-04 13:44:10 +0200

    [Testing]: Add Unit test with Junit-4.12

---
+++ LargestNumberTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import java.util.*;
+
+public class LargestNumberTest {
+  @Test
+  public void evaluatesList() {
+    LargestNumber testList = new LargestNumber();
+    List<Integer> testNumbers = Arrays.asList(420, 42, 423);
+    String testSrt = testList.findLargestNumber(testNumbers);
+    assertEquals("42423420", testSrt);
+  }
+}
